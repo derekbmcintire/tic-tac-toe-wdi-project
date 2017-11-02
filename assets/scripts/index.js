b621b08d2ca9b4e3319530e5464cef391c135fb0
derekbmcintire <derekbmcintire@gmail.com>
2017-11-02 10:46:11 -0400

    Write logic to track players moves
    Add basic logic to track players moves and determine if there is a
    winner.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,9 +6,6 @@ const events = require('./tic-tac-toe/events.js')
 
 $(() => {
   setAPIOrigin(location, config)
-  for (let i = 0; i < 9; i++) {
-    $('#' + i).on('click', events.onSquareClick)
-  }
 })
 
 // use require with a reference to bundle the file and use it in this file
--- assets/scripts/tic-tac-toe/events.js
+++ assets/scripts/tic-tac-toe/events.js
@@ -1,10 +1,58 @@
 let xTurn = true
+let winner = false
+let xTrack = []
+let oTrack = []
+const squares = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
+const win = [
+  ['0', '1', '2'],
+  ['3', '4', '5'],
+  ['6', '7', '8'],
+  ['0', '3', '6'],
+  ['1', '4', '7'],
+  ['2', '5', '8'],
+  ['0', '4', '8'],
+  ['2', '4', '6']
+]
 
+// switch turns between X and O
+const switchTurn = () => {
+  xTurn ? xTurn = false : xTurn = true
+}
+
+// track X and O moves in separate arrays
+const trackMove = function (square) {
+  if (xTurn) {
+    xTrack.push(square.id)
+  } else {
+    oTrack.push(square.id)
+  }
+}
+
+// add symbols to board and disable click function
 const onSquareClick = function () {
-  const currentSymbol = xTurn ? 'X' : 'O'
+  let currentSymbol = xTurn ? 'X' : 'O'
   $(this).text(currentSymbol)
   $(this).off('click')
-  xTurn ? xTurn = false : xTurn = true
+  trackMove(this)
+  switchTurn()
+  checkWin(xTrack)
+  checkWin(oTrack)
+}
+
+// add onClick event to all squares
+squares.map((x) => $('#' + x).on('click', onSquareClick))
+
+// check for win
+function checkWin (tracked) {
+  for (let i = 0; i < win.length; i++) {
+    if (
+      tracked.includes(win[i][0]) &&
+      tracked.includes(win[i][1]) &&
+      tracked.includes(win[i][2])
+    ) {
+      return (winner = true);
+    }
+  }
 }
 
 module.exports = {
